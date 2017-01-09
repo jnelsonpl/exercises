3c50ec62b386a262c93a037e713bbbf870df1df0
Jordan Nelson <jnelsontn@gmail.com>
2017-01-09 14:33:38 -0600

    Completed Exercise except enter key

--- the-static-web/converter/converter.js
+++ the-static-web/converter/converter.js
@@ -1,31 +1,46 @@
+var tempFahrenheit;
+var tempCelsius;
+var x = document.getElementById("converted_temperature");
+
 function toFahrenheit () {
 	var inputTemperature = parseFloat(document.getElementById("temperature").value);
-	var tempFahrenheit = ( (inputTemperature * 9) / 5) + 32;
-	
-	var x = document.getElementById("converted_temperature");
-	x.innerHTML = "The temperature in fahrenheit is: " + tempFahrenheit;
+	tempFahrenheit = ( (inputTemperature * 9) / 5) + 32;
+	tempFahrenheit = tempFahrenheit.toFixed(0);
+	colorTemperature();
+	x.innerHTML = "The temperature in fahrenheit is " + tempFahrenheit;
 }
 
 function toCelsius () {
 	var inputTemperature = parseFloat(document.getElementById("temperature").value);
-	var tempCelsius = ( (inputTemperature - 32) * 5) / 9;
-
-	var y = document.getElementById("converted_temperature");
-	y.innerHTML = "The temperature in celsius is: " + tempCelsius;
+	tempCelsius = ( (inputTemperature - 32) * 5) / 9;
+	tempCelsius = tempCelsius.toFixed(0);
+	colorTemperature();
+	x.innerHTML = "The temperature in celsius is " + tempCelsius;
 }
 
 // Get a reference to the button element in the DOM
 var button = document.getElementById("converter");
 
 // This function determines which radio button is selected and runs toFahrenheit or toCelsius
-function determineConverter () {
+function determineConverter() {
 	if (document.getElementById("fahrenheit").checked === true) {
 		toFahrenheit();
 	} else if (document.getElementById("celsius").checked === true) {
 		toCelsius();
-	} else {
-		alert("Select Fahrenheit or Celsius to convert temperature");
-	}
+	} else
+		alert("Please Select 'Fahrenheit' or 'Celsius' to convert");
+}
+
+// temperature > 90F/32C make it red
+// temperature < 32F/0C make it blue
+// else it is green
+function colorTemperature() {
+	if (tempFahrenheit > 90 || tempCelsius > 32)
+		x.style.color = "red";
+	else if (tempFahrenheit < 32 || tempCelsius < 0)
+		x.style.color = "blue";
+	else
+		x.style.color = "green";
 }
 
 // Add an event handler to the input field that checks if the user pressed the enter key, 
