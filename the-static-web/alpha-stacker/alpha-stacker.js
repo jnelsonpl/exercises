c23eb91c6e2e77a68316feb4f2a6776dc5ea3bc3
Jordan Nelson <jnelsontn@gmail.com>
2017-01-13 00:38:53 -0600

    alpha-stacker first commit

---
+++ the-static-web/alpha-stacker/alpha-stacker.js
@@ -0,0 +1,20 @@
+
+//var alphabet = [];
+var letters = "abcdefghijklmnopqrstuvwxyz"
+var alphabet = letters.split('');
+
+// The stackLetter function should accept the array as the sole argument
+function stackLetters (theAlphabetArray) {
+    /*
+      Write a `for` loop that iterates over the array argument and
+      outputs the letters.
+     */
+
+    for (var i = 0; i < letters.length; i++) {
+		theAlphabetArray.push(letters.substr(0, i));
+	}
+	console.log(theAlphabetArray);
+}
+
+// Invoke the function and pass in the array
+stackLetters(alphabet)
