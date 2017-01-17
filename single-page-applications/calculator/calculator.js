1ee5469b4dba52c20ea215ade428c7c2ae2fd57d
Jordan Nelson <jnelsontn@gmail.com>
2017-01-16 19:54:19 -0600

    Calculator Exercise Complete

--- single-page-applications/calculator/calculator.js
+++ single-page-applications/calculator/calculator.js
@@ -1,51 +1,64 @@
 
-/*
-Create variables for each button on the DOM
- */
+
+// Create variables for each button on the DOM
 var multiply = document.getElementById("multiply");
 var add = document.getElementById("add");
 var subtract = document.getElementById("subtract");
 var divide = document.getElementById("divide");
 
+// the element to display the answer
+var answer = document.getElementById("answerhere");
+
+// using global x/y valuables led to problems
+// var x = parseFloat(document.getElementById("fieldOne").value);
+// var y = parseFloat(document.getElementById("fieldTwo").value);
+
 /*
   Create a function that multiplies two numbers
   passed in as arguments. Return the product.
  */
-
-function multiplyTwo() {
-    var x = document.getElementById("fieldOne").value;
-    var y = document.getElementById("fieldTwo").value;
-    var z = x * y;
-    document.getElementById("answerhere").innerHTML = z;
+multiply.addEventListener("click", multiplyTwo);
+function multiplyTwo(x, y) {
+  x = parseFloat(document.getElementById("fieldOne").value);
+  y = parseFloat(document.getElementById("fieldTwo").value);
+  answer.innerHTML = x * y;
 };
 
 /*
   Create a function that adds two numbers
   passed in as arguments. Return the sum.
  */
-
-function addTwo(x, y) {
-    return x + y;
-}
+add.addEventListener("click", addTwo);
+function addTwo(x, y) { // We must use parseFloat or x + y will become "xy".
+                        // We do not seem to even need to add arg1, arg2
+  x = parseFloat(document.getElementById("fieldOne").value);
+  y = parseFloat(document.getElementById("fieldTwo").value);
+  answer.innerHTML = x + y;
+};
 
 
 /*
   Create a function that subtracts two numbers
   passed in as arguments. Return the difference.
  */
-
+subtract.addEventListener("click", subtractTwo);
 function subtractTwo(x, y) {
-  return x - y;
-}
+  x = parseFloat(document.getElementById("fieldOne").value);
+  y = parseFloat(document.getElementById("fieldTwo").value);
+  answer.innerHTML = x - y;
+};
 
 
 /*
   Create a function that divides two numbers
   passed in as arguments. Return the quotient.
  */
+divide.addEventListener("click", divideTwo);
 function divideTwo(x, y) {
-  return x / y;
-}
+  x = parseFloat(document.getElementById("fieldOne").value);
+  y = parseFloat(document.getElementById("fieldTwo").value);
+  answer.innerHTML = x / y;
+};
 
 
 /*
@@ -56,11 +69,14 @@ function divideTwo(x, y) {
 
   Return the value of the operation.
  */
+document.getElementById("addthenmultiply").addEventListener("click", addTwoNumbersThenMultiply);
 function addTwoNumbersThenMultiply(x, y, z) {
-    var theSum = x + y;
-    return theSum * z;
-}
-
+  x = parseFloat(document.getElementById("arg1").value);
+  y = parseFloat(document.getElementById("arg2").value);
+  z = x + y;
+  z = z * z;
+  document.getElementById("answerforthree").innerHTML = z;
+};
 
 
 
