7c29d6a4ad15ee014fbd0e63bd54efb4d269ff20
Jordan Nelson <jnelsontn@gmail.com>
2017-02-07 11:52:13 -0600

    probably should have approached a diff. way, more complex than needed

--- modern-javascript-developer/acme-explosives/javascripts/setup.js
+++ modern-javascript-developer/acme-explosives/javascripts/setup.js
@@ -1,47 +1,89 @@
 var loadedCategories;
 var loadedTypes;
 var loadedProducts;
+var whichone;
+
+var typesDiv = document.getElementById("types");
 
 getCategories().then(
-	function (resolve) {
+		function (resolve) {
 		loadedCategories = resolve;
-//		printCategories(loadedCategories);
+		// we are literally just ignoring this it seems ???
 });
 
 getTypes().then(
-	function (resolve) {
+		function (resolve) {
 		loadedTypes = resolve;
-//		printToPage(loadedTypes);
 });
 
 getProducts().then(
 	function (resolve) {
 		loadedProducts = resolve;
-		printProducts(loadedProducts);
 });
 
-function printCategories (loaded) {
-	for (var i = 0; i < loaded.length; i++) {
-		console.log(loaded[i].name + " " + loaded[i].id);
+let category_dropdown = document.getElementById("category-dropdown");
+category_dropdown.addEventListener("change", function () {
+	if (category_dropdown.value == 0) {
+		whichone = 0;
+	} else if (category_dropdown.value == 1) {
+		whichone = 1;
+	}
+	printType(whichone);
+});
+
+// We should probably then list out for each type...
+function printType (whichType) {
+	if (whichone == 0) {
+		for (var x in loadedTypes) {
+			if (loadedTypes[x].category === 0) {
+				//console.log(loadedTypes[x].name);
+
+				var namesOfTypes = loadedTypes[x];
+				var listOfTypes = "";
+
+				listOfTypes += `<div id="${namesOfTypes.name}">
+
+								${namesOfTypes.name}</div>
+								`
+				typesDiv.innerHTML += listOfTypes;
+			}
+		}
+	} else if (whichone == 1) {
+		for (var p in loadedTypes) {
+			if (loadedTypes[p].category == 1) {
+				//console.log(loadedTypes[p].name);
+
+				var namesOfTypes = loadedTypes[p];
+				var listOfTypes = "";
+
+				listOfTypes += `<div id="${namesOfTypes.name}">
+								${namesOfTypes.name}</div>
+								`
+				typesDiv.innerHTML += listOfTypes;
+			}
+		}
 	}
 }
+
+/*
+function printCategories (eachCategory) {
+	console.log(whichone);
+	for (var category in eachCategory) {
+			console.log("ID: " + eachCategory[category].id + " Category: " + eachCategory[category].name);
+	}
+} 
+*/
+
 
 function printProducts (eachProduct) {
 	for (var productKeys in eachProduct) {
 		let aProduct = eachProduct[productKeys];
 				for (var productInfo in aProduct) {
 					let productInfoComplete = aProduct[productInfo];
-					//console.log(productInfoComplete.name + " : " + productInfoComplete.description);
+					console.log(productInfoComplete.name + " : " + productInfoComplete.description);
 				}
 		}
-	}
-
-
-
-
-
-
-
+}
 
 
 
