9644e6b139831dbf81aa1addc1c9074a7d6ebaf0
Jordan Nelson <jnelsontn@gmail.com>
2017-02-10 11:15:32 -0600

    spent too much time already

--- musichistory/javascripts/filter.js
+++ musichistory/javascripts/filter.js
@@ -7,32 +7,8 @@
 
 function useFilter () {
 
-	var artistLi = $('.artist-li');
-	var artistSelected = $('#artist_dropdown').val();
 
-	for (var i = 0 ; i < artistLi.length; i++) {
-		if (artistSelected == artistLi[i].id) {
-			console.log(artistLi[i].id);
-			$(artistLi[i]).addClass('hidden');
-		}
-
-		
-		/* } else if (artistSelected !== artistLi[i].id) {
-			$('.artistLi').hide();
-		} */
-
-			
-
-		// (THIS) === THE DROP DOWN MENU
-		// hides the element selected
-		// $(artistLi[i]).parent().hide();
-		//artistLi[i].parentElement.classList.add('hidden');
-
-
-
-		}
-
-	}
+}
 
 
 
