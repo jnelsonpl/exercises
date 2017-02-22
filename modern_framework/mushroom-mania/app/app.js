011964600c5f5c540027cd019aa9b60c43d20ac1
Jordan Nelson <jnelsontn@gmail.com>
2017-02-22 00:01:12 -0600

    completed w/o firebase

---
+++ modern_framework/mushroom-mania/app/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var app = angular.module('Mushroom', ['ngRoute']);
+
+app.config( function ($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl: 'partials/mushroom-details.html',
+		controller: 'loadJsonCtrl'
+	});
+});
