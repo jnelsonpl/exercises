24828f255c248f5b0f543a574b4fd0eddecdf546
Jordan Nelson <jnelsontn@gmail.com>
2017-02-24 12:34:37 -0600

    mvp w/o firebase. json only. small styling update

--- modern_framework/doctors-and-patients/app/factories/loadDoctorsPatients.js
+++ modern_framework/doctors-and-patients/app/factories/loadDoctorsPatients.js
@@ -7,7 +7,7 @@ app.factory('loadDoctorsPatients', function ($q, $http, dataMassage) {
 			$http.get('./doctors-patients.json')
 			.then (function (dataReceived) {
 				// writing this function isn't needed...
-				// should have just split array into two
+				// should have set two variables in controller
 				var massaged = dataMassage.massageTheData(dataReceived);
 				resolve (massaged);
 			}).catch((error) => {
