dbb0f6baa5baa9b2047965684127f63d869104d6
Jordan Nelson <jnelsontn@gmail.com>
2017-02-27 20:21:55 -0600

    updating folder structure

--- modern-framework/mushroom-factory/app/app.js
+++ modern-framework/mushroom-factory/app/app.js
@@ -2,9 +2,9 @@
 
 var app = angular.module('Mushroom', ['ngRoute']);
 
-app.config( function ($routeProvider) {
+/* app.config( function ($routeProvider) {
 	$routeProvider.when('/', {
 		templateUrl: 'partials/mushroom-details.html',
 		controller: 'printToDomCtrl'
 	});
-});
+}); */
--- modern-framework/world_wide_travel/lib/Gruntfile.js
+++ modern-framework/world_wide_travel/lib/Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       options: {
-        predef: [ "document", "console", "$", "$scope", "firebase"],
+        predef: [ "document", "console", "$", "$scope" ],
         esnext: true,
         globalstrict: true,
         globals: {"angular": true, "app": true}
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files: {
-          '../css/styles.css': '../sass/styles.scss'
+          '../css/main.css': '../sass/main.scss'
         }
       }
     },
@@ -31,5 +31,5 @@ module.exports = function(grunt) {
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  grunt.registerTask('default', ['jshint', 'sass', 'watch']);
+  grunt.registerTask('default', ['jshint', 'watch']);
 };
